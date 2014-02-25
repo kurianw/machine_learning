b7eac8961bd7408de641c5dfd5e97e3e1124ebb4
Wilson Kurian <kurianw@gmail.com>
2014-02-24 23:48:19 -0800

    added files for linear regression

---
+++ linear_regression/linear_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class LinearRegression:
+    def correct_estimators(self, learning_rate, samples):
+       estimates = self.independent_variables.dot(self.estimators)
+       error = estimates - self.dependent_variable
+       gradient = self.independent_variables.T.dot(error)
+       self.estimators -= learning_rate/samples * gradient
+ 
+    def gradient_descent(self):
+       learning_rate = .01
+       samples =  self.independent_variables.shape[0]       
+       for iteration in xrange(3000):
+          self.correct_estimators(learning_rate, samples)
+
+    def feature_normalization(self, features):
+       return (features - self.means)/self.deviations
+ 
+    def __init__(self, dataset):
+        independent_variables = dataset[:,:-1] 
+        self.deviations, self.means = np.std(independent_variables, axis = 0), np.mean(independent_variables, axis = 0)
+        self.independent_variables = np.insert(self.feature_normalization(independent_variables), 0, 1, axis = 1)
+        self.dependent_variable= dataset[:,-1][np.newaxis].T
+        self.estimators = np.zeros((self.independent_variables.shape[1],1))
+        self.gradient_descent() 
+    
+    def predict(features):
+        normalized = feature_normalization(features)
+        return self.estimators.dot(np.insert(normalized, 1))
---
+++ linear_regression/univariate_regression.py
@@ -0,0 +1,7 @@
+import numpy as np
+from linear_regression import LinearRegression 
+
+numerify = lambda x: map(float, x.split(','))
+dataset = np.array(map(numerify, open('data/ex1data1.txt', 'r')))
+
+print LinearRegression(dataset).estimators 
