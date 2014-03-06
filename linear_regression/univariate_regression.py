a980776271c01c3343a37f84e1c1268eec0bfcf1
Wilson Kurian <kurianw@gmail.com>
2014-03-05 23:51:26 -0800

    removed useless print statements for a plot's x and y ranges

--- linear_regression/univariate_regression.py
+++ linear_regression/univariate_regression.py
@@ -8,9 +8,7 @@ class UnivariateRegression(LinearRegression):
         independent_variable = self.independent_variables[:,1]
         plt.scatter(independent_variable, self.dependent_variable, marker = 'x', color = 'r') 
         x_range = independent_variable.min(), independent_variable.max()
-        print x_range
         y_range = self.dependent_variable.min(), self.dependent_variable.max()
-        print y_range
         y_values = map(lambda x: self.predict([x]), x_range)
         plt.xlim(x_range[0] - 1, x_range[1] + 1)
         plt.ylim(y_range[0] - 1, y_range[1] + 1)
