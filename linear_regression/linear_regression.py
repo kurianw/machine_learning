corrected feature normalization in the prediction method

linear_regression/linear_regression.py
@@ -26,4 +26,4 @@ class LinearRegression:
     
     def predict(self,features):
         normalized = self.feature_normalization(features)
-        return np.insert(features, 0, 1).dot(self.estimators)
+        return np.insert(normalized, 0, 1).dot(self.estimators)[0]

linear_regression/multivariate_regression.py
@@ -4,4 +4,6 @@ from linear_regression import LinearRegression
 numerify = lambda x: map(float, x.split(','))
 dataset = np.array(map(numerify, open('data/ex1data2.txt', 'r')))
 
-print LinearRegression(dataset).estimators 
+lin = LinearRegression(dataset)
+print lin.estimators
+print lin.predict([1650,3])