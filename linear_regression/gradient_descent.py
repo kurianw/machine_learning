b7db15686a0ec6716fb1cd81f8b11d8a6b6d1972
Wilson Kurian <kurianw@kurianw-macbookpro2.roam.corp.google.com>
2016-12-28 13:32:20 -0800

    Created a handwriting recognition model using the TensorFlow MNIST tutorial.

--- linear_regression/gradient_descent.py
+++
@@ -1,4 +0,0 @@
-def gradient_descent(gradient, estimators, args=(), learning_rate = .01, iterations = 3000 ):
-    for iteration in xrange(iterations):
-        estimators -= learning_rate * gradient(estimators, *args)
-    return estimators
--- linear_regression/multivariate_regression.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from linear_regression import LinearRegression 
-
-numerify = lambda x: map(float, x.split(','))
-dataset = np.array(map(numerify, open('data/ex1data2.txt', 'r')))
-
-lin = LinearRegression(dataset)
-print lin.estimators
-print lin.predict([1650,3])
